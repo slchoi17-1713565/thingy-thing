d8e0e6f75e13d6d40bc93a4fe6ebb469f24584f0
slchoi17-1713565 <43793193+slchoi17-1713565@users.noreply.github.com>
2020-09-14 20:17:31 -0700

    Add files via upload

---
+++ Practice/Store.java
@@ -0,0 +1,37 @@
+public class Store {
+  // instance fields
+  String productType;
+  double price;
+
+  // constructor method
+  public Store(String product, double initialPrice) {
+    productType = product;
+    price = initialPrice;
+  }
+
+  // increase price method
+  public void increasePrice(double priceToAdd) {
+    double newPrice = price + priceToAdd;
+    price = newPrice;
+  }
+
+  // get price with tax method
+  public double getPriceWithTax() {
+    double totalPrice = price + (price * 0.08);
+    return totalPrice;
+  }
+
+  // toString() method
+  public String toString() {
+    return "This store sells " + productType + " at a price of " + price + ".";
+  }
+
+  // main method
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("lemonade", 3.75);
+    Store cookieShop = new Store("cookies", 2.50);
+    double lemonadePrice = lemonadeStand.getPriceWithTax();
+    System.out.println(lemonadeStand);
+    System.out.println(cookieShop);
+  }
+}
